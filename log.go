77ef0d2cdf5a2df6ca3378ac14ac06c7a608bb0f
Asif Mahmud Shimon <asif.shimon@technonext.com>
2024-10-29 18:28:17 +0600

    feat: added more configuration functions

--- log.go
+++ log.go
@@ -8,8 +8,11 @@ import (
 
 // LogOpt configuration options for settign up built-in loggers
 type LogOpt struct {
-	writer io.Writer
-	json   bool
+	writer     io.Writer
+	json       bool
+	handlerOpt *slog.HandlerOptions
+	replacers  map[string]AttrReplacerFunc
+	attrs      []slog.Attr
 }
 
 // LogOptFunc definition for configuration builder function
@@ -21,28 +24,43 @@ func Setup(funcs ...LogOptFunc) {
 	opt := &LogOpt{
 		writer: os.Stderr,
 		json:   true,
+		handlerOpt: &slog.HandlerOptions{
+			AddSource: false,
+			Level:     nil,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				return a
+			},
+		},
+		replacers: map[string]AttrReplacerFunc{},
+		attrs:     []slog.Attr{},
 	}
 
 	for _, f := range funcs {
 		opt = f(opt)
 	}
 
-	var h slog.Handler
-	ho := &slog.HandlerOptions{
-		AddSource: false,
-		Level:     nil,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			return a
-		},
+	opt.handlerOpt.ReplaceAttr = func(groups []string, a slog.Attr) slog.Attr {
+		if replacer, ok := opt.replacers[a.Key]; ok {
+			return replacer(a)
+		}
+
+		return a
 	}
 
+	var h slog.Handler
+
 	if !opt.json {
-		h = slog.NewTextHandler(opt.writer, ho)
+		h = slog.NewTextHandler(opt.writer, opt.handlerOpt)
 	} else {
-		h = slog.NewJSONHandler(opt.writer, ho)
+		h = slog.NewJSONHandler(opt.writer, opt.handlerOpt)
 	}
 
 	l := slog.New(h)
+
+	for _, attr := range opt.attrs {
+		l = l.With(attr)
+	}
+
 	slog.SetDefault(l)
 }
 
@@ -58,6 +76,41 @@ func WithWriter(wr io.Writer) LogOptFunc {
 func WithText() LogOptFunc {
 	return func(lo *LogOpt) *LogOpt {
 		lo.json = false
+		return lo
+	}
+}
+
+// WithSource adds caller information in slog's log
+func WithSource() LogOptFunc {
+	return func(lo *LogOpt) *LogOpt {
+		lo.handlerOpt.AddSource = true
+		return lo
+	}
+}
+
+// WithLevel sets log level
+func WithLevel(l slog.Level) LogOptFunc {
+	return func(lo *LogOpt) *LogOpt {
+		lo.handlerOpt.Level = l
+		return lo
+	}
+}
+
+// AttrReplacerFunc function definition for replacing attributes
+type AttrReplacerFunc func(slog.Attr) slog.Attr
+
+// WithReplacer adds an attribute replacer to customize formatting
+func WithReplacer(key string, replacer AttrReplacerFunc) LogOptFunc {
+	return func(lo *LogOpt) *LogOpt {
+		lo.replacers[key] = replacer
+		return lo
+	}
+}
+
+// WithAttr adds an attribute in all of the logs
+func WithAttr(attr slog.Attr) LogOptFunc {
+	return func(lo *LogOpt) *LogOpt {
+		lo.attrs = append(lo.attrs, attr)
 		return lo
 	}
 }
--- log_test.go
+++ log_test.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"log"
 	"log/slog"
+	"strings"
 	"testing"
+	"time"
 
 	golog "github.com/asif-mahmud/go-log"
 )
@@ -43,6 +45,230 @@ func TestDefault(t *testing.T) {
 
 			if m.Msg != cs.msg {
 				t.Errorf("expected: %s, found: %s", cs.msg, m.Msg)
+			}
+
+			buf.Truncate(0)
+		})
+	}
+}
+
+func TestWithSource(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	golog.Setup(golog.WithWriter(buf), golog.WithSource())
+
+	type testCase struct {
+		msg     string
+		logFunc func(string, ...any)
+	}
+
+	cases := []testCase{
+		{"Hello, World", slog.Warn},
+		{"Hello, World", slog.Info},
+	}
+
+	type msg struct {
+		Msg    string      `json:"msg"`
+		Source slog.Source `json:"source"`
+	}
+
+	for _, cs := range cases {
+		t.Run(cs.msg, func(t *testing.T) {
+			cs.logFunc(cs.msg)
+
+			var m msg
+
+			if err := json.NewDecoder(buf).Decode(&m); err != nil {
+				t.Error(err)
+			}
+
+			if m.Msg != cs.msg {
+				t.Errorf("expected: %s, found: %s", cs.msg, m.Msg)
+			}
+
+			if !strings.HasSuffix(m.Source.File, "log_test.go") {
+				t.Error("source.file is wrong:", m.Source.File)
+			}
+
+			buf.Truncate(0)
+		})
+	}
+}
+
+func TestWithLevel(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	golog.Setup(golog.WithWriter(buf), golog.WithLevel(slog.LevelWarn))
+
+	type testCase struct {
+		msg         string
+		logFunc     func(string, ...any)
+		shouldMatch bool
+	}
+
+	cases := []testCase{
+		{"Hello, World", slog.Debug, false},
+		{"Hello, World", slog.Info, false},
+		{"Hello, World", slog.Warn, true},
+	}
+
+	type msg struct {
+		Msg string `json:"msg"`
+	}
+
+	for _, cs := range cases {
+		t.Run(cs.msg, func(t *testing.T) {
+			cs.logFunc(cs.msg)
+
+			var m msg
+
+			if !cs.shouldMatch && buf.Len() > 0 {
+				t.Errorf("should've omitted log, but logged")
+			}
+
+			if !cs.shouldMatch {
+				return
+			}
+
+			if err := json.NewDecoder(buf).Decode(&m); err != nil {
+				t.Error(err)
+			}
+
+			if cs.shouldMatch && m.Msg != cs.msg {
+				t.Errorf("expected: %s, found: %s", cs.msg, m.Msg)
+			}
+
+			if !cs.shouldMatch && m.Msg == cs.msg {
+				t.Errorf("expected: , found: %s", m.Msg)
+			}
+
+			buf.Truncate(0)
+		})
+	}
+}
+
+func TestWithReplacers(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	levels := map[string]string{
+		"DEBUG": "debug",
+		"INFO":  "info",
+	}
+
+	golog.Setup(
+		golog.WithWriter(buf),
+		golog.WithLevel(slog.LevelDebug),
+
+		// replaces key only
+		golog.WithReplacer(slog.TimeKey, func(a slog.Attr) slog.Attr {
+			return slog.Attr{
+				Key:   "timestamp",
+				Value: a.Value,
+			}
+		}),
+
+		// replaces both key and value
+		golog.WithReplacer(slog.LevelKey, func(a slog.Attr) slog.Attr {
+			// t.Error(a.Value, a.Value.Kind(), a.Value.Resolve().String())
+			if level, ok := levels[a.Value.String()]; ok {
+				return slog.Attr{
+					Key:   "lvl",
+					Value: slog.StringValue(level),
+				}
+			}
+
+			return slog.Attr{
+				Key:   "lvl",
+				Value: a.Value,
+			}
+		}),
+	)
+
+	type testCase struct {
+		msg     string
+		logFunc func(string, ...any)
+		level   string
+	}
+
+	cases := []testCase{
+		{"Hello, World", slog.Debug, "debug"},
+		{"Hello, World", slog.Info, "info"},
+		{"Hello, World", slog.Warn, "WARN"},
+	}
+
+	type msg struct {
+		Msg       string    `json:"msg"`
+		Timestamp time.Time `json:"timestamp"`
+		Level     string    `json:"lvl"`
+	}
+
+	for _, cs := range cases {
+		t.Run(cs.msg, func(t *testing.T) {
+			cs.logFunc(cs.msg)
+
+			var m msg
+
+			if err := json.NewDecoder(buf).Decode(&m); err != nil {
+				t.Error(err)
+			}
+
+			if cs.msg != m.Msg {
+				t.Errorf("expected: %s, found: %s", cs.msg, m.Msg)
+			}
+
+			if m.Timestamp.IsZero() {
+				t.Errorf("invalid timestamp, got: %v", m.Timestamp)
+			}
+
+			if cs.level != m.Level {
+				t.Errorf("expected: %s, found: %s", cs.level, m.Level)
+			}
+
+			buf.Truncate(0)
+		})
+	}
+}
+
+func TestWithAttribute(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	golog.Setup(golog.WithWriter(buf), golog.WithAttr(slog.Attr{
+		Key:   "service",
+		Value: slog.StringValue("my-service"),
+	}))
+
+	type testCase struct {
+		msg     string
+		logFunc func(string, ...any)
+		service string
+	}
+
+	cases := []testCase{
+		{"Hello, World", slog.Warn, "my-service"},
+		{"Hello, World", slog.Info, "my-service"},
+	}
+
+	type msg struct {
+		Msg     string `json:"msg"`
+		Service string `json:"service"`
+	}
+
+	for _, cs := range cases {
+		t.Run(cs.msg, func(t *testing.T) {
+			cs.logFunc(cs.msg)
+
+			var m msg
+
+			if err := json.NewDecoder(buf).Decode(&m); err != nil {
+				t.Error(err)
+			}
+
+			if m.Msg != cs.msg {
+				t.Errorf("expected: %s, found: %s", cs.msg, m.Msg)
+			}
+
+			if m.Service != cs.service {
+				t.Errorf("expected: %s, found: %s", cs.service, m.Service)
 			}
 
 			buf.Truncate(0)
